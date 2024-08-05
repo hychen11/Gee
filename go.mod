8f0990788284d26c0d52bac66d4f9004f4139d6a
hychen11 <zjuchy1@gmail.com>
2024-08-06 00:10:55 +0800

    http: add router table

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module example
 go 1.21.3
 
 replace gee => ./gee
+
+require gee v0.0.0-00010101000000-000000000000 // indirect
--- main.go
+++ main.go
@@ -2,26 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"gee"
 )
 
-type Engine struct{}
 
-func (engine *Engine) ServeHTTP(w http.ResponseWriter,r *http.Request){
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprintf(w, "URL.PATH = %q\n",r.URL.Path)
-	case "/hello":
-		for k,v :=range r.Header{
-			fmt.Fprintf(w, "Header[%q]=%q\n",k,v)
-		}
-	default:
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n",r.URL)
-	}
-}
 
 func main(){
-	engine:=new(Engine)
-	log.Fatal(http.ListenAndServe(":9999",engine))
+	r:=gee.New()
+	r.GET("/",func(w http.ResponseWriter, req *http.Request){
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+	r.GET("/hello",func(w http.ResponseWriter, req *http.Request){
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+	r.Run(":9999")
 }
